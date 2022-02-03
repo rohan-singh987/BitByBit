cd924c8c49efa3d33e01b9cff1056c11b727a315
Rohan Singh <rohannsingh98765@gmail.com>
2022-02-04 02:14:55 +0530

    carousel
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -14,32 +14,42 @@ const Carousel = () => {
         autoplay: true
     }
 
-  return <div className="">
+  return <div className="bg-black p-24">
+
 
       <div>
-          <h1 className="flex justify-center content-center font-bold text-4xl">
+          <h1 className="flex justify-center content-center font-bold text-4xl text-white">
               Carousel
           </h1>
       </div>
+
+      <div className="bg-gray-50 py-10">
+
       
       <Slider {...settings} >
-            <div className="content-center mx-72">
-                <div className="box bg-[#ed8611]">
+            <div className="content-center mx-64">
+                <div className="box bg-[#ed8611] flex flex-col justify-center p-16">
+                    <h2>Lorem Ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt corporis soluta eos laborum non dolorum accusantium? Similique velit dignissimos dicta nulla laboriosam sit, deleniti possimus aliquid, natus aut eligendi at.</p>
+                </div>
+            </div>
+            <div className="content-center mx-64">
+                <div className="box bg-[#f76565] flex flex-col justify-center p-16">
+                <h2>Lorem Ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt corporis soluta eos laborum non dolorum accusantium? Similique velit dignissimos dicta nulla laboriosam sit, deleniti possimus aliquid, natus aut eligendi at.</p>
 
                 </div>
             </div>
-            <div className="content-center mx-72">
-                <div className="box bg-[#f76565] ">
-
-                </div>
-            </div>
-            <div className="content-center mx-72">
-                <div className="box bg-[#60f2f2] ">
+            <div className="content-center mx-64">
+                <div className="box bg-[#60f2f2] flex flex-col justify-center p-16 ">
+                <h2>Lorem Ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt corporis soluta eos laborum non dolorum accusantium? Similique velit dignissimos dicta nulla laboriosam sit, deleniti possimus aliquid, natus aut eligendi at.</p>
 
                 </div>
             </div>
             
         </Slider>
+      </div>
 
   </div>;
 };
